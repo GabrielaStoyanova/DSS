50bb2c02c420d979f31a6be4920566ea914aff66
Gaby <gabigs2002@gmail.com>
2023-07-12 19:47:21 +0300

    test

--- Homework 1/Exercise 1.js
+++ Homework 1/Exercise 1.js
@@ -4,8 +4,26 @@ const person = [{ name : "Joe", age: 20, hasChildren: true},
 { name : "Katty", age: 23, hasChildren: false},
 { name : "Betty", age: 33, hasChildren: false}]; 
 
+//First Solution
 let fPerson = person.length;
 
 for (let i = 0; i < fPerson; i++) {
-    console.log(person[i]);
+    if(person.age > 35){
+        console.log(person[i]);
+    }
+}
+
+//Second Solution 
+const newPerson = person.filter(function (el) {
+    return el.age > 35;
+}
+);
+console.log(newPerson);
+
+//Third Solution 
+let fnewPerson = newPerson.length;
+
+for (let i = 0; i < fnewPerson; i++) {
+        document.getElementById("demo").innerHTML = JSON.stringify(newPerson);
   }
+
