9c3c86b3b0403d85834d3f34e8265bb9e3a1a026
Gaby <gabigs2002@gmail.com>
2023-07-12 18:18:54 +0300

    test

---
+++ Homework 1/Exercise 1.js
@@ -0,0 +1,11 @@
+const person = [{ name : "Joe", age: 20, hasChildren: true}, 
+{ name : "Mike", age: 50, hasChildren: false},
+{ name : "Ivan", age: 45, hasChildren: true},
+{ name : "Katty", age: 23, hasChildren: false},
+{ name : "Betty", age: 33, hasChildren: false}]; 
+
+let fPerson = person.length;
+
+for (let i = 0; i < fPerson; i++) {
+    console.log(person[i]);
+  }
